a03abf40f0bb3ca4b7bf6baadbdc3c3af5e02849
pm <info@dasstoffwerk.de>
2025-12-24 10:28:26 +0100

    Init sw_ext_purchase

---
+++ modules/sw_ext_purchase/tests/test_module.py
@@ -0,0 +1,10 @@
+
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class SwExtPurchaseTestCase(ModuleTestCase):
+    "Test Sw Ext Purchase module"
+    module = 'sw_ext_purchase'
+
+
+del ModuleTestCase
